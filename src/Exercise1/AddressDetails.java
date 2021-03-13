8ddf9e3b9f7cdfbe0b4678f51a4735d3a6eb7c18
mrsameer <sameershaik2001@outlook.com>
2021-03-13 20:40:59 +0530

    Added AddressDetails.java

---
+++ src/Exercise1/AddressDetails.java
@@ -0,0 +1,10 @@
+package Exercise1;
+
+public class AddressDetails {
+    public static void main(String[] args) {
+        System.out.println("Door No: D089");
+        System.out.println("Street: St. Louis Street");
+        System.out.println("City: Springfield");
+        System.out.println("ZIP Code: 62729");
+    }
+}

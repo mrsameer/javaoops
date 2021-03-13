39c3db1ccfbcde7258a60ffcd4223314ea065fa6
mrsameer <sameershaik2001@outlook.com>
2021-03-13 20:38:33 +0530

    added TraineeDetails.java

---
+++ src/Exercise1/TraineeDetails.java
@@ -0,0 +1,12 @@
+/*
+ Create a new Java project with "TraineeDetails.java" file and implement a Java code to display your name
+ and designation in two separate lines. .
+ */
+package Exercise1;
+
+public class TraineeDetails {
+    public static void main(String[] args) {
+        System.out.println("My name is Maria Jerome");
+        System.out.println("My Designation is Systems Engineer");
+    }
+}

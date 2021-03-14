7c98432a70f32674f8fae891d4125ae656fcb845
mrsameer <sameershaik2001@outlook.com>
2021-03-14 22:12:30 +0530

    Create file Test2.java in constructors package

---
+++ src/constructors/Test2.java
@@ -0,0 +1,12 @@
+package constructors;
+
+public class Test2 {
+    // Name of the class and constructor must be same
+    Test2() {
+        System.out.println("This is constructor");
+    }
+
+    public static void main(String[] args) {
+        Test2 t = new Test2();
+    }
+}

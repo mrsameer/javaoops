4498961070f7ad3ed437b8556f62e1a968dcccfb
mrsameer <sameershaik2001@outlook.com>
2021-03-14 22:51:26 +0530

    Create file Test5.java in constructors package

---
+++ src/constructors/Test5.java
@@ -0,0 +1,10 @@
+// Demonstration of compiler generates default constructor
+package constructors;
+
+public class Test5 {
+    public static void main(String[] args) {
+        // Here new keyword creates object and
+        // compiler adds default constructor.
+        Test5 t = new Test5();
+    }
+}

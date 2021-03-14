4b1cdc2b62807631d44fddac13e510fc3d6da9fb
mrsameer <sameershaik2001@outlook.com>
2021-03-14 22:14:36 +0530

    Create file Test3.java in constructors package

---
+++ src/constructors/Test3.java
@@ -0,0 +1,14 @@
+package constructors;
+
+public class Test3 {
+    // Return type concept is not valid for constructor
+    // If we declare return type then compiler considers it as method.
+    void Test3() {
+        System.out.println("This is method");
+    }
+
+    public static void main(String[] args) {
+        Test3 t = new Test3();
+        t.Test3();
+    }
+}

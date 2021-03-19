b26f6c59f2d660c3d4d8092741390af20c46a44b
mrsameer <sameershaik2001@outlook.com>
2021-03-19 16:22:55 +0530

    added gui.awt.MyFrame13 Example on Menubar, Menu, MenuItem GUI components

---
+++ src/gui/awt/MyFrame13.java
@@ -0,0 +1,73 @@
+package gui.awt;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyFrame13 extends Frame implements ActionListener {
+    String item="";
+    public MyFrame13() {
+        // set properties of the frame
+        this.setVisible(true);
+        this.setBackground(Color.green);
+        this.setSize(500, 500);
+        this.setTitle("Menu Example");
+
+        // add window listener
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+
+        // create menubar component
+        MenuBar mb = new MenuBar();
+
+        // add menubar to this frame
+        this.setMenuBar(mb);
+
+        // create menu component
+        Menu m  = new Menu("File");
+        mb.add(m);
+
+        // create menuitems and add to Menu
+        MenuItem mi1 = new MenuItem("New");
+        MenuItem mi2 = new MenuItem("Open");
+        MenuItem mi3 = new MenuItem("Save");
+        MenuItem mi4 = new MenuItem("Save As");
+
+        // add action listener to the menuitems
+        mi1.addActionListener(this);
+        mi2.addActionListener(this);
+        mi3.addActionListener(this);
+        mi4.addActionListener(this);
+
+        // add menuitems to menu
+        m.add(mi1);
+        m.add(mi2);
+        m.add(mi3);
+        m.add(mi4);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        item = e.getActionCommand();
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        Font f = new Font("arial", Font.BOLD, 30);
+        g.setFont(f);
+        g.drawString("Selected item: " + item, 50, 300);
+    }
+}
+
+class MyFrame13Demo {
+    public static void main(String[] args) {
+        MyFrame13 f = new MyFrame13();
+    }
+}

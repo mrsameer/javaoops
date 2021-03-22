bcd29e50e514b4958cb90f1474f5a929dffe5e96
mrsameer <sameershaik2001@outlook.com>
2021-03-23 04:41:21 +0530

    Completed 1st question in week5

--- src/labmanual/week5/Q1.java
+++ src/labmanual/week5/Q1.java
@@ -4,4 +4,25 @@
 package labmanual.week5;
 
 public class Q1 {
+    public static void main(String[] args) {
+        // create primitive types
+        int a = 5;
+        double b = 5.54;
+
+        // convert primitive types info wrapper objects (boxing)
+        Integer aObj = Integer.valueOf(a);
+        Double bObj = Double.valueOf(b);
+
+        if (aObj instanceof Integer)
+            System.out.println("An Integer object is created");
+        if (bObj instanceof Double)
+            System.out.println("A Double object is created");
+
+        // convert objects into primitive types (unboxing)
+        int aint = aObj.intValue();
+        double bdoub = bObj.doubleValue();
+
+        System.out.println("The value of aint " + aint);
+        System.out.println("The value of bdub " + bdoub);
+    }
 }
